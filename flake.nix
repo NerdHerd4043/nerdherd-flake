cb58c456d86e7202c89877636cd00fea0c692121
NullCube <nullcub3@gmail.com>
2024-10-13 17:00:25 -0700

    Flake: Init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Home Manager configuration for nerdherd4043";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    {
+      homeConfigurations = {
+        "nerdherd4043" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [
+              inputs.agenix.overlays.default
+              inputs.rust-overlay.overlays.default
+            ];
+          };
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [
+            ./home.nix
+          ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
+      };
+    };
+}
