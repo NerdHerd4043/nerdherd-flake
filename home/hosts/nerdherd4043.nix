9dfe2db16b976bcdaf3bc46c61a74af5eb2bec0e
NullCube <nullcub3@gmail.com>
2025-03-26 01:48:53 -0700

    Home: adjust stateVersion; use nixfmt

--- home/hosts/nerdherd4043.nix
+++ home/hosts/nerdherd4043.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   # Allow installing any non-free (not fully open source) packages
   nixpkgs.config.allowUnfree = true;
@@ -63,7 +63,7 @@
         openssl.dev
         pkg-config
         nil
-        nixpkgs-fmt
+        nixfmt
       ]
     );
   };
