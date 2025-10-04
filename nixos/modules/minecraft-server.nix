8e04b2bc0299d904cc6eb8cb2118188719c55d79
NullCube <nullcub3@gmail.com>
2025-10-04 03:12:19 -0700

    minecraft-server: add options, configure service

--- nixos/modules/minecraft-server.nix
+++ nixos/modules/minecraft-server.nix
@@ -9,12 +9,24 @@ let
   cfg = config.herd.minecraft-server;
   inherit (lib)
     mkEnableOption
+    mkPackageOption
+    mkOption
     mkIf
+    types
     ;
 in
 {
   options.herd.minecraft-server = {
     enable = mkEnableOption "herd minecraft-server";
+
+    server = mkPackageOption pkgs [ "papermcServers" "papermc-1_21_4" ] { };
+    mem = mkOption {
+      type = types.str;
+      default = "5G";
+    };
+    dir = mkOption {
+      default = "/home/minecraft/1-21-4-paper";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -34,15 +46,19 @@ in
       allowedTCPPorts = [ 4043 ];
     };
 
+    environment.systemPackages = with pkgs; [
+      screen
+    ];
+
+    systemd.tmpfiles.rules = [
+      # Create directory if it doesn't exist already
+      "d '${cfg.dir}'      0755 minecraft minecraft - -"
+    ];
+
     # TODO: Set up a not-jank service
     systemd.services = {
       minecraft =
         let
-          # Server config
-          server = pkgs.papermcServers.papermc-1_21_4;
-          mem = "5G";
-          dir = "/home/minecraft/1.21.4paper";
-
           screen = "${pkgs.screen}/bin/screen";
           cmd = cmd: "${screen} -S minecraft -X stuff \"${cmd}\"^M";
         in
@@ -56,8 +72,8 @@ in
 
           script = ''
             ${screen} -DmS minecraft "${pkgs.writeShellScript "start-minecraft" ''
-              ${server}/bin/minecraft-server \
-              -Xms${mem} -Xmx${mem} -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+              ${cfg.server}/bin/minecraft-server \
+              -Xms${cfg.mem} -Xmx${cfg.mem} -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
               -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
               -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 \
               -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 \
@@ -77,13 +93,14 @@ in
             ${cmd "stop"}
           '';
 
+          wantedBy = [ "multi-user.target" ];
+          restartIfChanged = false;
           serviceConfig = {
             User = "minecraft";
             Group = "minecraft";
-            WorkingDirectory = dir;
+            WorkingDirectory = cfg.dir;
             Restart = "on-failure";
           };
-          wantedBy = [ "multi-user.target" ];
         };
     };
   };
