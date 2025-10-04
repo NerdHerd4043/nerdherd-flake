aa2ac7659688c40d6231d66f45ebce45ebfc0541
NullCube <nullcub3@gmail.com>
2025-10-04 02:42:56 -0700

    Secrets: add caveserver and rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   # Systems
   poppy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILhJ0QdHoDe0agOMZwpF6uEn3LwEUeYFGAAh50fptosL";
-  caveserver = "";
+  caveserver = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYPeJmSKp9WHnu1u+lB/s7v6uB6fN6lbaDHPHQePfKR";
 
   # Users (for acccess)
   nullcube = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ8qUgVoBKq5DdokVxzqQmIbrpkvp09s8o3PjAO3HuLr";
@@ -9,6 +9,7 @@ let
   keys = [
     # Systems
     poppy
+    caveserver
 
     # Users (for access)
     nullcube
