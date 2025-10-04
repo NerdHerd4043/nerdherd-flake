51887baeb5e07fdfc14b2847a565ff1259bcf6ef
NullCube <nullcub3@gmail.com>
2025-10-04 03:53:30 -0700

    ddclient: increase interval, set username to token

--- nixos/modules/ddclient.nix
+++ nixos/modules/ddclient.nix
@@ -24,11 +24,12 @@ in
     services.ddclient = {
       enable = true;
       verbose = true;
-      interval = "3min";
+      interval = "5min";
       ssl = true;
       usev4 = "webv4, webv4='https://cloudflare.com/cdn-cgi/trace', webv4-skip='ip= '";
       protocol = "cloudflare";
       zone = "nerdherd4043.org";
+      username = "token";
       passwordFile = config.age.secrets.cf-dns-token.path;
       domains = [ config.networking.fqdn ];
     };
